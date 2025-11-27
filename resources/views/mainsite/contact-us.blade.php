9a2c4d4fee84e38bce019dd918dcd96151ee92fc
_AditiDubey <157035164+Unyc1124@users.noreply.github.com>
2025-11-27 14:00:18 +0530

    Update postal code in contact us page

--- resources/views/mainsite/contact-us.blade.php
+++ resources/views/mainsite/contact-us.blade.php
@@ -76,7 +76,7 @@
                                 <div class="listtype">
                                     <a>
                                         Mind Rewire, Park Ivory, Park Street, Wakad, Pune, Maharashtra, India -
-                                        411057
+                                        421501
                                     </a>
                                 </div>
                             </div>
