986728fcc74a4003ae6ed813345ebdc9e511e16d
Anand Singh <92910742+Gttpoc@users.noreply.github.com>
2025-12-02 13:06:48 +0530

    test

--- resources/views/mainsite/contact-us.blade.php
+++ resources/views/mainsite/contact-us.blade.php
@@ -76,7 +76,7 @@
                                 <div class="listtype">
                                     <a>
                                         Mind Rewire, Park Ivory, Park Street, Wakad, Mumbai, Maharashtra, India -
-                                        411057
+                                        400000
                                     </a>
                                 </div>
                             </div>
